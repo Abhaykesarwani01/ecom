aeb6cb37044bed4e00bc119de63896b2d9121036
Abhay Kesarwani <103197529+Abhaykesarwani01@users.noreply.github.com>
2025-10-07 08:59:00 +0000

    test 2

--- ecom-api/src/services/user.service.js
+++ ecom-api/src/services/user.service.js
@@ -1,10 +1,12 @@
+import User from "../models/user.model.js";
+
 const createUser = async(userData)=>{
     try {
         let {firstName,lastname,email,password}=userData;
 
         const isUserExist =  await User.findOne({email:email});
         if(isUserExist){
-            throw new Error("User already exists");
+            throw new Error("User already exists",email);
         }
 
     } catch (error) {
